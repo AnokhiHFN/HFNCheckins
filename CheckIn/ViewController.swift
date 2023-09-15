3013e54028da2741c41d922b923cc345b4378491
Anokhi <anokhias@gmail.com>
2023-09-15 17:57:54 +0200

    Initial Commit

---
+++ CheckIn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CheckIn
+//
+//  Created by Anokhi Shah on 15.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
