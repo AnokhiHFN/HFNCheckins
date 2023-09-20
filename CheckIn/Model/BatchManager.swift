4e45f3421f2d88ce73ac8602873f24f14ab2eaea
Anokhi <anokhias@gmail.com>
2023-09-20 19:51:18 +0200

    mainscreen active

---
+++ CheckIn/Model/BatchManager.swift
@@ -0,0 +1,12 @@
+//
+//  BatchManager.swift
+//  CheckIn
+//
+//  Created by Anokhi Shah on 20.09.23.
+//
+
+import Foundation
+
+struct BatchManager {
+    let batchArray = ["Batch-1", "Batch-2", "Batch-1 & 2"]
+}
