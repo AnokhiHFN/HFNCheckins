f64a54197f9a8c3176c6e53d759642ea1576eeeb
Anokhi <anokhias@gmail.com>
2023-10-02 13:38:11 +0200

    fix: confetti starts behind the big circle

--- CheckIn/Controller/FinalScreenViewController.swift
+++ CheckIn/Controller/FinalScreenViewController.swift
@@ -17,7 +17,7 @@ class FinalScreenViewController: UIViewController {
         view.addSubview(backgroundImage)
 
         // Create a SwiftUI view for confetti
-        let confettiView = ConfettiView(colors: [.red, .blue, .green], number: 50, speed: 2.0)
+        let confettiView = ConfettiView(colors: [.red, .blue, .green], number: 200, speed: 5.0)
 
         // Create a UIHostingController for the SwiftUI view
         let hostingController = UIHostingController(rootView: AnyView(confettiView))
@@ -30,5 +30,23 @@ class FinalScreenViewController: UIViewController {
         addChild(hostingController)
         view.addSubview(hostingController.view)
         hostingController.didMove(toParent: self)
+
+        // Create a big circle view
+        let circleView = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        circleView.center = view.center
+        circleView.backgroundColor = UIColor(named: "formColor") // Set the background color to your desired color
+        circleView.layer.cornerRadius = circleView.frame.width / 2
+
+        // Create a checkmark (tick) view
+        let checkmarkView = UIImageView(image: UIImage(systemName: "checkmark.circle.fill"))
+        checkmarkView.frame = circleView.bounds
+        checkmarkView.contentMode = .scaleAspectFit
+        checkmarkView.tintColor = .white
+
+        // Add the checkmark to the circle view
+        circleView.addSubview(checkmarkView)
+
+        // Add the circle view as a subview to the view controller's view
+        view.addSubview(circleView)
     }
 }
--- CheckIn/View/Confetti.swift
+++ CheckIn/View/Confetti.swift
@@ -8,41 +8,67 @@ struct ConfettiView: View {
     var body: some View {
         ZStack {
             ForEach(0..<number, id: \.self) { _ in
-                ConfettiPiece(colors: colors, speed: speed)
+                Group {
+                    if Bool.random() {
+                        CircleConfettiPiece(colors: colors, speed: speed)
+                    } else {
+                        RectangleConfettiPiece(colors: colors, speed: speed)
+                    }
+                }
             }
         }
     }
 }
 
-struct ConfettiPiece: View {
+struct CircleConfettiPiece: View {
     let colors: [Color]
     let speed: Double
     
     @State private var rotation: Double = .random(in: 0...360)
-    @State private var position: CGPoint = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2) // Center
+    @State private var position: CGPoint = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 3) // Center
     
     var body: some View {
         ZStack {
-            Rectangle()
-                .fill(Color.clear) // Set the background color to clear
-                .frame(width: 10, height: 20)
-                .rotationEffect(.degrees(rotation))
-                .position(position)
-            
-            // Add the confetti shape (you can replace this with your desired shape)
-            Image(systemName: "sparkles")
-                .foregroundColor(colors.randomElement() ?? .red)
-                .frame(width: 10, height: 20)
+            Circle()
+                .fill(colors.randomElement() ?? .red) // Fill with a random color from your array
+                .frame(width: 2, height: 2) // Adjust the size of the circle
                 .rotationEffect(.degrees(rotation))
                 .position(position)
         }
         .background(Color.clear) // Set the ZStack's background color to clear
         .onAppear {
             withAnimation(Animation.linear(duration: speed).repeatForever(autoreverses: false)) {
-                self.rotation += .random(in: -45...45) // Random rotation
+                self.rotation += .random(in: -360...360) // Random rotation
                 self.position.x = .random(in: 0...UIScreen.main.bounds.width) // Random horizontal position
                 self.position.y = .random(in: 0...UIScreen.main.bounds.height) // Random vertical position
             }
         }
     }
 }
+
+struct RectangleConfettiPiece: View {
+    let colors: [Color]
+    let speed: Double
+    
+    @State private var rotation: Double = .random(in: 0...360)
+    @State private var position: CGPoint = CGPoint(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 3) // Center
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+                .fill(colors.randomElement() ?? .red) // Fill with a random color from your array
+                .frame(width: 5, height: 5) // Adjust the size of the rectangle
+                .rotationEffect(.degrees(rotation))
+                .position(position)
+        }
+        .background(Color.clear) // Set the ZStack's background color to clear
+        .onAppear {
+            withAnimation(Animation.linear(duration: speed).repeatForever(autoreverses: false)) {
+                self.rotation += .random(in: -360...360) // Random rotation
+                self.position.x = .random(in: 0...UIScreen.main.bounds.width) // Random horizontal position
+                self.position.y = .random(in: 0...UIScreen.main.bounds.height) // Random vertical position
+            }
+        }
+    }
+}
+
