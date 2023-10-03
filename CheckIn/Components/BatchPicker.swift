ef3ca861380366bc11f7cb2baddcb8995f23da12
Anokhi <anokhias@gmail.com>
2023-10-03 16:20:00 +0200

    fix: seperated into different views and components

---
+++ CheckIn/Components/BatchPicker.swift
@@ -0,0 +1,8 @@
+//
+//  BatchPicker.swift
+//  CheckIn
+//
+//  Created by Anokhi Shah on 03.10.23.
+//
+
+import Foundation
---
+++ CheckIn/Components/TitleLabel.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// Create a reusable title label component
+class TitleLabel: UILabel {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureLabel()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureLabel()
+    }
+    
+    private func configureLabel() {
+        translatesAutoresizingMaskIntoConstraints = false
+        text = "68th Birthday Celebrations of\nPujya Daaji Maharaja" // Use '\n' for line break
+        textAlignment = .center
+        font = UIFont.systemFont(ofSize: 24, weight: .bold) // Increase font size
+        numberOfLines = 2 // Allow for multiple lines
+        adjustsFontSizeToFitWidth = true
+        textColor = UIColor(named: "buttonColor") // Set the text color to "buttonColor"
+    }
+}
