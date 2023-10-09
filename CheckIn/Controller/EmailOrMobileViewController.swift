b4ae4a01dc23fc4ba7c4a61c91652b8de229d2a2
Anokhi <anokhias@gmail.com>
2023-10-09 19:15:50 +0200

    fix: checkin forms have functional fields now

--- CheckIn/Controller/EmailOrMobileViewController.swift
+++ CheckIn/Controller/EmailOrMobileViewController.swift
@@ -51,27 +51,33 @@ class EmailOrMobileViewController: UIViewController, CheckInFormDelegate {
             get: { self.givenMobile ?? "" },
             set: { self.givenMobile = $0 }
         )
-
+        
         // Create a SwiftUI view with the binding
-        var swiftUIView = SwiftUIView(batch: batchBinding, email: emailBinding, mobile: mobileBinding)
-        swiftUIView.delegate = self
-        
-        // Embed the SwiftUI view within a UIHostingController
-        let hostingController = UIHostingController(rootView: swiftUIView)
-        
-        // Add the UIHostingController as a child view controller
-        addChild(hostingController)
-        
-        // Set the frame for the SwiftUI view
-        hostingController.view.frame = view.bounds
-        
-        // Add the SwiftUI view as a subview
-        view.addSubview(hostingController.view)
-        
-        // Notify the child view controller that it has been added to the parent view controller
-        hostingController.didMove(toParent: self)
-        
-        // Adjust the navigation bar title
-        navigationItem.title = "Check-In"
+        if #available(iOS 14.0, *) {
+            var swiftUIView = SwiftUIView(batch: batchBinding, email: emailBinding, mobile: mobileBinding)
+            swiftUIView.delegate = self
+            
+            // Embed the SwiftUI view within a UIHostingController
+            let hostingController = UIHostingController(rootView: swiftUIView)
+            
+            // Add the UIHostingController as a child view controller
+            addChild(hostingController)
+            
+            // Set the frame for the SwiftUI view
+            hostingController.view.frame = view.bounds
+            
+            // Add the SwiftUI view as a subview
+            view.addSubview(hostingController.view)
+            
+            // Notify the child view controller that it has been added to the parent view controller
+            hostingController.didMove(toParent: self)
+            
+            // Adjust the navigation bar title
+            navigationItem.title = "Check-In"
+        }
+        else {
+            // Fallback on earlier versions
+            print("Not supported")
+        }
     }
 }
