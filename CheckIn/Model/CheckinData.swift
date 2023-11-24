7770b6831ffecd0f2b1cf990fb972461d4a328b4
Anokhi <anokhias@gmail.com>
2023-11-24 15:53:33 +0100

    fix: abhyasi id manual part done

--- CheckIn/Model/CheckinData.swift
+++ CheckIn/Model/CheckinData.swift
@@ -36,3 +36,25 @@ struct CheckInData: Codable {
     }
     
 }
+
+struct CheckInDataID: Codable {
+
+    // QR Type special data
+    var abhyasiId: String //correct
+    var batch: String // correct
+    var dormAndBerthAllocation: String? // correct
+    var eventName = "68th Birthday Celebration of Pujya Daaji Maharaj"
+    var timestamp: String // correct
+    var type = "ABHYASI_ID" // correct
+    
+    
+    func asDictionary() throws -> [String: Any] {
+        let encoder = JSONEncoder()
+        let data = try encoder.encode(self)
+        guard let dictionary = try JSONSerialization.jsonObject(with: data, options: .allowFragments) as? [String: Any] else {
+            throw NSError(domain: "Invalid data", code: 0, userInfo: nil)
+        }
+        return dictionary
+    }
+    
+}
