ba6e8f60be2fdb55a29c7b709d1b3f840100ed3e
Anokhi <anokhias@gmail.com>
2024-01-25 19:09:46 +0100

    fix: app works well, need more testing

---
+++ CheckIn/Controller/ContainerViewController.swift
@@ -0,0 +1,79 @@
+import UIKit
+import FirebaseFirestore
+
+class ContainerViewController: UIViewController {
+
+    var eventTitles: [String]?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Add image in the middle of the screen
+        let imageView = UIImageView(image: UIImage(named: "hfn"))
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(imageView)
+
+        NSLayoutConstraint.activate([
+            imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            imageView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            imageView.widthAnchor.constraint(equalToConstant: 400), // Adjust the width as needed
+            imageView.heightAnchor.constraint(equalToConstant: 400) // Adjust the height as needed
+        ])
+
+        fetchEvents { [weak self] titles in
+            guard let self = self else { return }
+            
+            self.eventTitles = titles
+            
+            // Check the number of events and perform the segue accordingly
+            if titles.count == 1 {
+                // Only one title, directly perform the segue to EntryViewController
+                let selectedTitle = titles.first!
+                self.performSegue(withIdentifier: "EntrySegue", sender: selectedTitle)
+            } else {
+                self.performSegue(withIdentifier: "EventSegue", sender: self)
+            }
+        }
+    }
+
+    // Add the fetchEvents function here
+    func fetchEvents(completion: @escaping ([String]) -> Void) {
+        let eventsCollection = Firestore.firestore().collection("ongoing-events")
+
+        eventsCollection.getDocuments { (snapshot, error) in
+            if let error = error {
+                print("Error getting documents: \(error)")
+                completion([]) // Pass an empty array in case of error
+            } else {
+                if let snapshot = snapshot {
+                    var titles: [String] = []
+                    for document in snapshot.documents {
+                        if let title = document["title"] as? String {
+                            titles.append(title)
+                        }
+                    }
+                    completion(titles)
+                } else {
+                    print("Snapshot is nil")
+                    completion([]) // Pass an empty array if snapshot is nil
+                }
+            }
+        }
+    }
+
+
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EntrySegue", let selectedTitle = sender as? String {
+            if let entryViewController = segue.destination as? EntryViewController {
+                entryViewController.selectedEventTitle = selectedTitle
+            }
+        }
+        if segue.identifier == "EventSegue", let eventListViewController = segue.destination as? EventListViewController {
+            // Pass the eventTitles to EventListViewController
+            eventListViewController.eventTitles = eventTitles
+        }
+    }
+
+}
---
+++ CheckIn/Controller/EventListViewController.swift
@@ -0,0 +1,126 @@
+//
+//  EventListViewController.swift
+//  CheckIn
+//
+//  Created by Anokhi Shah on 23.01.24.
+//
+
+import UIKit
+
+class EventListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    var eventTitles: [String]?
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        // Set additional properties if needed
+        return tableView
+    }()
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Hide the back button
+        self.navigationItem.setHidesBackButton(true, animated: false)
+
+        // Set up background image
+        let backgroundImage = UIImage(named: "Background") // Make sure to add your image to the Assets.xcassets
+        let backgroundImageView = UIImageView(image: backgroundImage)
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.frame = view.bounds
+        view.addSubview(backgroundImageView)
+        view.sendSubviewToBack(backgroundImageView)
+        
+        
+        // Create and configure the table view
+        //tableView = UITableView()
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.separatorStyle = .none
+        // Set corner radius for all four corners
+        tableView.layer.cornerRadius = 10
+        tableView.register(EventCell.self, forCellReuseIdentifier: "EventCell") // Register the custom cell
+        tableView.backgroundColor = UIColor.clear // Set background color if needed
+
+        // Add the table view to the view hierarchy
+        view.addSubview(tableView)
+
+        // Do any additional setup after loading the view.
+        // Apply constraints to the table view (adjust these as needed)
+         tableView.translatesAutoresizingMaskIntoConstraints = false
+         NSLayoutConstraint.activate([
+             tableView.topAnchor.constraint(equalTo: view.topAnchor, constant: 300), // Adjust the top padding
+             tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20), // Adjust the left padding
+             tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20), // Adjust the right padding
+             tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100) // Adjust the bottom padding
+         ])
+
+    }
+    
+    // Add this UITableViewDelegate method to set the cell height
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        // Set your preferred cell height here
+        return 60.0
+    }
+    
+    // MARK: - UITableViewDataSource
+
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Add 1 to the count for the title cell
+        return (eventTitles?.count ?? 0) + 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventCell", for: indexPath)
+        
+        // Customize the appearance based on the cell index
+        if indexPath.row == 0 {
+            // First cell is the title cell
+            cell.textLabel?.text = "Select Event"
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 20)
+            cell.textLabel?.textColor = UIColor.black
+        } else {
+            // Other cells are event cells
+            cell.textLabel?.text = eventTitles?[indexPath.row - 1]
+            cell.textLabel?.textColor = UIColor.black
+        }
+        // Set corner radius for cell
+        cell.contentView.layer.cornerRadius = 10
+        cell.contentView.layer.masksToBounds = true
+        
+        
+        return cell
+    }
+    
+    // MARK: - UITableViewDelegate
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // Check if a row is selected and it is not the title cell
+        if indexPath.row > 0, let selectedTitle = eventTitles?[indexPath.row - 1] {
+            // Perform the segue to EntryViewController
+            print("selected title is \(selectedTitle)")
+            UserDefaults.standard.set(selectedTitle, forKey: "SelectedEventTitle")
+            performSegue(withIdentifier: "EventToEntry", sender: selectedTitle)
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
+        // Prevent selection of the title cell
+        return indexPath.row == 0 ? nil : indexPath
+    }
+    
+    // ... Additional UITableViewDelegate methods can be implemented as needed
+    
+    // MARK: - Navigation
+    
+    // Prepare for segue to pass the selected title
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "EventToEntry", let selectedTitle = sender as? String {
+            if let entryViewController = segue.destination as? EntryViewController {
+                entryViewController.selectedEventTitle = selectedTitle
+            }
+        }
+    }
+    
+}
